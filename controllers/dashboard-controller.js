a76ccd15ffb3e5a9673975c5872f1c2b47eba711
Rajkumar-Javiya <javiyaraj12@gmail.com>
2023-03-16 18:28:22 +0530

    23DEV041 Question Screen Complete

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,7 +1,39 @@
-class StdentQuestion{
-    static UserQuestion= async(req,res)=>{
-        res.render('questions');
+const dbHelper = require('../database/dbHelper.js');
+const queryExecurter = require('../database/dbHelper.js');
+class StdentQuestion {
+    static UserQuestion = async (req, res) => {
+
+        const questionQuery = `SELECT * FROM exam_admin.question_master;`
+        const allQuestions = await queryExecurter(questionQuery);
+        const questions = [];
+
+        for (let i = 0; i < allQuestions.length; i++) {
+
+            const question_options = await queryExecurter(`SELECT * FROM exam_admin.option_master where question_id=${allQuestions[i].question_id}`);
+
+            var options = [];
+
+            var trueOption=[];
+
+            for (let j = 0; j < question_options.length; j++) {
+                if(allQuestions[i].question_answer == question_options[j].option_value){
+                    trueOption.push(true);
+                }else{
+                    trueOption.push(false);
+                }
+                options[j] = question_options[j].option_value;
+            }
+
+            questions[i] = {
+                "question": allQuestions[i].question,
+                "answer": allQuestions[i].question_answer,
+                "option": options,
+                "correct_ans":trueOption
+            }
+        }
+        res.render('questions', { questions: questions });
     }
 }
 
-module.exports=StdentQuestion;
+
+module.exports = StdentQuestion;
---
+++ database/dbHelper.js
@@ -0,0 +1,13 @@
+const con = require('../database/dbconnect.js');
+
+const queryExecurter = (query) => {
+    return new Promise((resolve, reject) => {
+        con.query(query, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        })
+    })
+}
+module.exports=queryExecurter;
