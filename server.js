b525d0a2e30f2b911946f6f3a8f81517d22751df
rjcoding <rjcoding123@gmail.com>
2023-03-18 01:09:14 +0530

    23DEV015 dashboard ui change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { ejs } = require('ejs');
 var http = require('http');
 const { connect } = require('http2');
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
 const con = mysql2.createConnection({
@@ -17,6 +17,7 @@ const con = mysql2.createConnection({
 });
 
 con.connect();
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
@@ -31,23 +32,45 @@ const queryExecutor = (query) => {
         });
     })
 }
-var data=[];
-app.get('/dashboard', async(req,res)=>{
-    var categories = await queryExecutor(`select count(category_id) as totalcategories from question_category`);
-    data[0]=categories[0].totalcategories;
-    // console.log(categories);
-    var exam = await queryExecutor(`select count(exam_id) as total from exam_master`);
-//    console.log(exam);
-   data[1] = exam[0].total;
-   var que = await queryExecutor(`select count(question_id) as totalque from question_master`);
-   data[2] = que[0].totalque;
-//    console.log(que);
-   var user = await queryExecutor(`select count(user_id) as totaluser from user_master`);
-   data[3] = user[0].totaluser;
-//    console.log(user);
-    res.render('dashboard.ejs',{data: data});
+
+app.get('/dashboard', async (req, res) => {
+
+    var data = [];
+
+    var categories = await queryExecutor(`select count(*) as totalCategories from exam_admin.question_category`);
+    data[0] = categories[0].totalCategories;
+
+    var exam = await queryExecutor(`select count(*) as exams from exam_admin.exam_master`);
+    data[1] = exam[0].exams;
+
+    var que = await queryExecutor(`select count(*) as totalQuestion from exam_admin.question_master`);
+    data[2] = que[0].totalQuestion;
+    
+    var user = await queryExecutor(`SELECT count(*) as students FROM exam_admin.student_master;`);
+    data[3] = user[0].students;
+
+
+
+    var category=await queryExecutor(`SELECT * FROM exam_admin.question_category;`);
+
+    data[4]=category;
+
+    let questionsRatio=[];
+    for(let i=0;i<data[4].length;i++){
+    
+        const id=data[4][i].category_id;
+
+        var que = await queryExecutor(`SELECT count(*) as questions FROM exam_admin.question_master where question_master.category_id=${id}`);
+        
+        const questionCount=que[0].questions;
+        questionsRatio[i]=`width:${Math.floor((questionCount/data[4].length)*10)}%`;
+    }
+
+    const exam_attempt=await queryExecutor(`SELECT * FROM exam_admin.exam_attempt_master`);
+    
+    res.render('dashboard.ejs', { data: data,questionsRatio:questionsRatio,exam_attempt:exam_attempt });
 })
-app.listen(5001, function(){
-    console.log('Server is running on port 5000');
+app.listen(5001, function () {
+    console.log('Server is running on port 5001');
 })
 
