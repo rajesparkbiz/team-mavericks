2f865dcd7e558040d32c50305eb3f22ba55a1b38
Rajkumar-Javiya <javiyaraj12@gmail.com>
2023-03-17 18:21:32 +0530

    23DEV041 update in  question crud screen

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -3,6 +3,10 @@ const queryExecurter = require('../database/dbHelper.js');
 class StdentQuestion {
     static UserQuestion = async (req, res) => {
 
+        const optionTitle=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+        const question_category=await queryExecurter(`SELECT question_category.category_name FROM exam_admin.question_category;`);
+        
         const questionQuery = `SELECT * FROM exam_admin.question_master;`
         const allQuestions = await queryExecurter(questionQuery);
         const questions = [];
@@ -25,13 +29,14 @@ class StdentQuestion {
             }
 
             questions[i] = {
+                "question_id":allQuestions[i].question_id,
                 "question": allQuestions[i].question,
                 "answer": allQuestions[i].question_answer,
                 "option": options,
                 "correct_ans":trueOption
             }
         }
-        res.render('questions', { questions: questions });
+        res.render('questions', { questions: questions ,optionTitle,categories:question_category});
     }
 }
 
--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-const express=require('express');
+const express = require('express');
 require('dotenv').config();
-const ejs=require('ejs');
-const bodyParser=require('body-parser');
-const authRoute=require('./routes/auth-route.js');
-const dashboardRoute=require('./routes/dashboard-route.js');
-const port=process.env.PORT;
-const app=express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const authRoute = require('./routes/auth-route.js');
+const dashboardRoute = require('./routes/dashboard-route.js');
+const queryExecurter=require('./database/dbHelper.js')
+const port = process.env.PORT;
+const app = express();
 
-app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/auth',authRoute);
-app.use('/dashboard',dashboardRoute);
-app.use(express.static(__dirname+"/public"))
+app.set('view engine', 'ejs')
 
-app.listen(port,()=>{
+app.use('/auth', authRoute);
+app.use('/dashboard', dashboardRoute);
+
+app.use(express.static(__dirname + "/public"))
+
+
+app.get('/question/data/one', async (req, res) => {
+    const id = req.query.id;
+
+    const questionData = await queryExecurter(`SELECT * FROM exam_admin.question_master where question_master.question_id=${parseInt(id)}`);
+
+    const questionoption=await queryExecurter(`SELECT option_master.option_id,option_master.option_value FROM exam_admin.option_master where option_master.question_id=${parseInt(id)}`);
+
+    res.json({ questionData:questionData[0],questionOption:questionoption});
+})
+
+app.post('/question/update',(req,res)=>{
+    console.log(req.body);
+})
+
+app.listen(port, () => {
     console.log(`Server is Running on ${port}`);
 })
 
