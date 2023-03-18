cbcf82e78006ee3b6e67a1fdb16c454dee0e8fae
mohitdangariya13 <mohit.dangariya.espark2023@gmail.com>
2023-03-18 17:43:09 +0530

    23DEV062 exam toggle feature adedd

--- server.js
+++ server.js
@@ -9,41 +9,57 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 var connection = mysql.createConnection({
-
-  host: 'localhost',
-
-  user: 'root',
-
-  password: 'root',
-
-  database: 'exam_admin'
-
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'exam_admin'
 });
 
 const queryExecutor = (query) => {
-    return new Promise((resolve,rejects) => {
-        connection.query(query,(err,result) => {
+    return new Promise((resolve, rejects) => {
+        connection.query(query, (err, result) => {
             resolve(result);
         });
     })
 }
 
-app.get('/dashboard', async(req, res) => {
- res.render('index.ejs');
+app.get('/dashboard', async (req, res) => {
+    res.render('index.ejs');
 })
-app.get('/users',async(req,res) => {
+
+app.get('/users', async (req, res) => {
     var student_master = await queryExecutor(`SELECT * FROM student_master`);
-    res.render('user.ejs',{data:student_master});
+    res.render('user.ejs', { data: student_master });
 
 })
-app.get('/exam',async(req,res) => {
+
+app.get('/exam', async (req, res) => {
     var exam_master = await queryExecutor(`SELECT * FROM exam_master`);
-    res.render('exam.ejs',{data:exam_master});
+    res.render('exam.ejs', { data: exam_master });
 })
-// app.get("/user",async(req,res)=>{
-//  res.render('user.ejs');
-// })
-app.listen(3000,function() {
+
+app.get('/exam/update', async (req, res) => {
+
+    const currentStatus = await queryExecutor(`SELECT exam_master.exam_isActive as status FROM exam_admin.exam_master where exam_master.exam_id=1`);
+
+
+    const isActive = currentStatus[0].status;
+
+    var query;
+    if (isActive == 'yes') {
+
+        query = `update exam_master set exam_isActive = 'no' where exam_id=${req.query.id}`;
+
+    } else {
+        query = `update exam_master set exam_isActive = 'yes' where exam_id=${req.query.id}`;
+    }
+
+    const toogleSwitchQuesry = await queryExecutor(query);
+    res.redirect('/exam');
+
+})
+
+app.listen(3000, function () {
     console.log('Connected  on port 3000');
 })
 
