83b7a471be37f3a8bfab328dd5a7581b7fb12fcd
Rajkumar-Javiya <javiyaraj12@gmail.com>
2023-03-23 14:03:39 +0530

    23DEV041 code structure change

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -5,21 +5,21 @@ class StdentQuestion {
     static dashboardPage = async(req,res)=>{
         var data = [];
 
-        var categories = await queryExecurter(`select count(*) as totalCategories from exam_admin.question_category`);
+        var categories = await queryExecurter(`select count(*) as totalCategories from question_category`);
         data[0] = categories[0].totalCategories;
     
-        var exam = await queryExecurter(`select count(*) as exams from exam_admin.exam_master`);
+        var exam = await queryExecurter(`select count(*) as exams from exam_master`);
         data[1] = exam[0].exams;
     
-        var que = await queryExecurter(`select count(*) as totalQuestion from exam_admin.question_master`);
+        var que = await queryExecurter(`select count(*) as totalQuestion from question_master`);
         data[2] = que[0].totalQuestion;
     
-        var user = await queryExecurter(`SELECT count(*) as students FROM exam_admin.student_master;`);
+        var user = await queryExecurter(`SELECT count(*) as students FROM student_master;`);
         data[3] = user[0].students;
     
     
     
-        var category = await queryExecurter(`SELECT * FROM exam_admin.question_category;`);
+        var category = await queryExecurter(`SELECT * FROM question_category;`);
     
         data[4] = category;
     
@@ -29,13 +29,13 @@ class StdentQuestion {
     
             const id = data[4][i].category_id;
     
-            var que = await queryExecurter(`SELECT count(*) as questions FROM exam_admin.question_master where question_master.category_id=${id}`);
+            var que = await queryExecurter(`SELECT count(*) as questions FROM question_master where question_master.category_id=${id}`);
     
             const questionCount = que[0].questions;
             questionsRatio[i] = `width:${Math.floor((questionCount / data[4].length) * 10)}%`;
         }
     
-        const exam_attempt = await queryExecurter(`SELECT * FROM exam_admin.exam_attempt_master`);
+        const exam_attempt = await queryExecurter(`SELECT * FROM exam_attempt_master`);
         var s_id = req.body.student_id;
     
         var student_name = await queryExecurter(`select student_master.fname from student_master inner join  exam_attempt_master on exam_attempt_master.student_id=student_master.student_id `);
