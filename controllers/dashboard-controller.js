b3caabc9af592b74792edfab277d42a54e677116
Rajkumar-Javiya <javiyaraj12@gmail.com>
2023-03-30 12:25:47 +0530

    23DEV041 pagination error solve

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -2,96 +2,104 @@ const queryExecurter = require('../database/dbHelper.js');
 const con = require("../database/dbconnect.js")
 class StdentQuestion {
 
-    
-    static dashboardPage = async(req,res)=>{
+
+    static dashboardPage = async (req, res) => {
         var data = [];
 
         var categories = await queryExecurter(`select count(*) as totalCategories from question_category`);
         data[0] = categories[0].totalCategories;
-    
+
         var exam = await queryExecurter(`select count(*) as exams from exam_master`);
         data[1] = exam[0].exams;
-    
+
         var que = await queryExecurter(`select count(*) as totalQuestion from question_master`);
         data[2] = que[0].totalQuestion;
-    
+
         var user = await queryExecurter(`SELECT count(*) as students FROM student_master;`);
         data[3] = user[0].students;
-    
-    
-    
+
+
+
         var category = await queryExecurter(`SELECT * FROM question_category;`);
-    
+
         data[4] = category;
-    
+
         let questionsRatio = [];
 
         for (let i = 0; i < data[4].length; i++) {
-    
+
             const id = data[4][i].category_id;
-    
+
             var que = await queryExecurter(`SELECT count(*) as questions FROM question_master where question_master.category_id=${id}`);
-    
+
             const questionCount = que[0].questions;
             questionsRatio[i] = `width:${Math.floor((questionCount / data[4].length) * 10)}%`;
         }
-    
+
 
 
         const exam_action = await queryExecurter(`SELECT exam_master.exam_name,exam_master.exam_isActive,exam_master.action_time FROM test.exam_master;`);
 
-        var exam_data=[];
-        for(let i=0;i<exam_action.length;i++){
-            const date=new Date(exam_action[i].action_time);
+        var exam_data = [];
+        for (let i = 0; i < exam_action.length; i++) {
+            const date = new Date(exam_action[i].action_time);
             var status;
 
-            if(exam_action[i].exam_isActive=='yes'){
-                status='Active'
+            if (exam_action[i].exam_isActive == 'yes') {
+                status = 'Active'
             }
-            if (exam_action[i].exam_isActive=='no'){
-                status='InActive'   
+            if (exam_action[i].exam_isActive == 'no') {
+                status = 'InActive'
             }
 
-            exam_data[i]={
-                "exam_name":exam_action[i].exam_name,
-                "exam_isActive":status,
-                "action_time":date.toLocaleString()
+            exam_data[i] = {
+                "exam_name": exam_action[i].exam_name,
+                "exam_isActive": status,
+                "action_time": date.toLocaleString()
             }
         }
 
 
-        res.render('dashboard', { data: data, questionsRatio: questionsRatio,exam_action:exam_data});
+        res.render('dashboard', { data: data, questionsRatio: questionsRatio, exam_action: exam_data });
     }
 
-    static displayExams=async(req,res)=>{
-    
+    static displayExams = async (req, res) => {
+
         var limit = 5;
         var page = req.query.page || 1;
-        var offset = (page-1) * limit;
+        var offset = (page - 1) * limit;
         var ajax = req.query.AJAX || false;
-        var exam_data  ;
-         con.query(`SELECT * FROM exam_master LIMIT ${offset},${limit};`,(err,exam_master)=>{
-            if(err) throw err;
 
-            con.query('SELECT count(*) as count from exam_master;',(err,result)=>{
-                if(err) throw err;
-                var count = Math.ceil((result[0].count)/limit);
-                
-                exam_data = exam_master 
 
-                if(!ajax)
-                {
-                    res.render('exam.ejs', { data: exam_master, count , column_name: "" });
-                }else{
-                    res.json(exam_data);
-                }
-            
-            })
-        })
+        const exam_data = await queryExecurter(`SELECT * FROM exam_master LIMIT ${offset},${limit};`);
+        var status=[];
+        for(let i=0;i<exam_data.length;i++){
+            const questionCount=await queryExecurter(`SELECT count(*) as count FROM test.exam_category where exam_id=${exam_data[i].exam_id}`);
+            if(parseInt(questionCount[0].count)>0){
+                status[i]=true;
+            }else{
+                status[i]=false;
+            }
+        }
 
-        
+        const result = await queryExecurter(`SELECT count(*) as count from exam_master;`);
+
+        var count = Math.ceil((result[0].count) / limit);
+
+        console.log(count);
+
+
+
+        if (!ajax) {
+            res.render('exam.ejs', { data: exam_data, count,status:status});
+        } else {
+            res.json({exam_data,status});
+        }
+
+
+
     }
-    
+
 }
 
 
